b18bc4c8a82ecad00d3de45454237cfa1ffa50ee
Dileepreddy-13 <dr.2006lokasani@gmail.com>
2025-07-11 19:52:39 +0530

    parking status

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-// Toggle menubar slide in/out
 const menuIcon = document.getElementById('menu');
 const menubar = document.querySelector('.menubar');
 
 menuIcon.addEventListener('click', function() {
-    menubar.classList.toggle('open');
+    menubar.classList.add('open');
 });
 
-// Optional: click outside to close
 document.addEventListener('click', function(e) {
     if (!menubar.contains(e.target) && e.target !== menuIcon) {
         menubar.classList.remove('open');
     }
 });
+
+const welcome = document.querySelector('.welcome');
+const park=document.getElementById('park-opt');
+const remove=document.getElementById('remove-opt');
+const stat=document.getElementById('status-opt');
+
+park.addEventListener('click',function(){
+    menubar.classList.add("hide");
+    welcome.classList.add("hide");
+})
+remove.addEventListener('click',function(){
+    menubar.classList.add("hide");
+    welcome.classList.add("hide");
+})
+stat.addEventListener('click',function(){
+    menubar.classList.add("hide");
+    welcome.classList.add("hide");
+})
